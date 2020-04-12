87cf78675ef02bf31a8b5539e170e07750c56c6f
Artem Orlov <art.orlov@mail.ru>
2020-04-12 14:06:49 +0500

    Updates

---
+++ precache-manifest.65869b640d2c028ec14767c9063ecab9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75ec1f82def4153f2d37a037d827b68",
+    "url": "/siapp/index.html"
+  },
+  {
+    "revision": "e91c1ccda34e0e3db23a",
+    "url": "/siapp/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "2b7f51f7785ba3ab54f2",
+    "url": "/siapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e91c1ccda34e0e3db23a",
+    "url": "/siapp/static/js/2.4930ff82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/siapp/static/js/2.4930ff82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7f51f7785ba3ab54f2",
+    "url": "/siapp/static/js/main.5809c44b.chunk.js"
+  },
+  {
+    "revision": "0f59f99cf55d094fa70f",
+    "url": "/siapp/static/js/runtime-main.5e26e7d8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/siapp/static/media/persik.4e1ec840.png"
+  }
+]);
