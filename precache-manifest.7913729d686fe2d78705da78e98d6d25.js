35376620184f61450f283bb5960fe769e969d90e
Artem Orlov <art.orlov@mail.ru>
2020-04-19 00:39:11 +0500

    Updates

--- precache-manifest.7913729d686fe2d78705da78e98d6d25.js
+++ precache-manifest.7913729d686fe2d78705da78e98d6d25.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91d893a2a15ac18bf9ebe6a882949811",
+    "revision": "6d38f6e236baedebb6c46860add6cdfe",
     "url": "/siapp/index.html"
   },
   {
-    "revision": "5c045861c511d4955c99",
+    "revision": "dbec29c3d399a4596d0e",
     "url": "/siapp/static/css/2.4fc5a6bc.chunk.css"
   },
   {
-    "revision": "bd88a4870c8b16173ed6",
+    "revision": "7dac84836931e9223628",
     "url": "/siapp/static/css/main.0238e034.chunk.css"
   },
   {
-    "revision": "5c045861c511d4955c99",
-    "url": "/siapp/static/js/2.44ccd6d1.chunk.js"
+    "revision": "dbec29c3d399a4596d0e",
+    "url": "/siapp/static/js/2.430b8069.chunk.js"
   },
   {
     "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
-    "url": "/siapp/static/js/2.44ccd6d1.chunk.js.LICENSE.txt"
+    "url": "/siapp/static/js/2.430b8069.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bd88a4870c8b16173ed6",
-    "url": "/siapp/static/js/main.79fd25ec.chunk.js"
+    "revision": "7dac84836931e9223628",
+    "url": "/siapp/static/js/main.8a5a9ed9.chunk.js"
   },
   {
     "revision": "0f59f99cf55d094fa70f",
